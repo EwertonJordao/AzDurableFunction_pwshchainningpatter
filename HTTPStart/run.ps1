05446f3dc49c97e38cef1401665caa07b32e1fc8
Ewerton R Jordao <ewertonjordao@gmail.com>
2020-09-02 20:58:51 -0300

    remove file sample

--- HTTPStart/run.ps1
+++ HTTPStart/run.ps1
@@ -3,7 +3,8 @@ using namespace System.Net
 param($Request, $TriggerMetadata)
 
 $FunctionName = $Request.Params.FunctionName
-$InstanceId = Start-NewOrchestration -FunctionName $FunctionName
+Write-Host $Request 
+$InstanceId = Start-NewOrchestration -FunctionName $FunctionName  
 Write-Host "Started orchestration with ID = '$InstanceId'"
 
 $Response = New-OrchestrationCheckStatusResponse -Request $Request -InstanceId $InstanceId
